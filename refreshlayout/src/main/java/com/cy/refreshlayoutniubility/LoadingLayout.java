cd74c19553ff021019979f025e671cf64a89426a
437042721@qq.com <437042721@qq.com>
2020-12-12 10:52:24 +0800

    修改loadinglayout

--- refreshlayout/src/main/java/com/cy/refreshlayoutniubility/LoadingLayout.java
+++ refreshlayout/src/main/java/com/cy/refreshlayoutniubility/LoadingLayout.java
@@ -35,8 +35,8 @@ public class LoadingLayout extends FrameLayout {
 
     public LoadingLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        loadingView = new ThreeScaleCircleView(context);
-        layoutParams_loading = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        loadingView = new RotateLineCircleView(context);
+        layoutParams_loading = new LayoutParams(ScreenUtils.dpAdapt(context,30),ScreenUtils.dpAdapt(context,30));
         layoutParams_loading.gravity = Gravity.CENTER;
 //        setLoadingView(loadingView,layoutParams_loading);
     }
